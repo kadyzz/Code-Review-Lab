216d2708f26ec31f8c014cf24be7f6bf216d80f3
Kadriye Yildiz <36813519+kadyzz@users.noreply.github.com>
2020-12-03 15:30:50 +0100

    Add files via upload
    fixed!

--- search.java
+++ search.java
@@ -11,29 +11,29 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class search {
-	
-	public static void main(String[] args) throws IOException{
-		
-		Scanner sc = new Scanner(System.in); 
-		
-		System.out.println("Enter a string: ");
-	    String str = sc.nextLine(); //takes in the user input in the form "search [word] [file]"
-	    
-	    String[] arr = str.split(" "); //splits the whole string into single words in an array to pinpoint file and given word separately
 
-	    String word = arr[1]; //the specified word to search for
-	    String file = arr[2]; //the specified file to search the word in 
-	    
-	    BufferedReader br = new BufferedReader(new FileReader(file)); //start reading of file 
-	    String line;
-	    while( (line = br.readLine()) != null) //read every line until text ends 
-	        {
-	            if (line.indexOf(word) != -1) //if the word matches in a line
-	                {
-	                    System.out.println(line); //print the whole line 
-	                }
-	        }
-	    br.close( );
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string: ");
+		String str = sc.nextLine(); // takes in the user input in the form "search [word] [file]"
+
+		String[] arr = str.split(" "); // splits the whole string into single words in an array to pinpoint file and
+										// given word separately
+
+		String word = arr[1]; // the specified word to search for
+		String file = arr[2]; // the specified file to search the word in
+
+		BufferedReader br = new BufferedReader(new FileReader(file)); // start reading of file
+		String line;
+		while ((line = br.readLine()) != null) // read every line until text ends
+		{
+			if (line.indexOf(word) != -1) // if the word matches in a line
+			{
+				System.out.println(line); // print the whole line
+			}
+		}
+		br.close();
 	}
 }
-
